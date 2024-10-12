c679e2a56b2f98c659386830a9a95b2ffee606c5
Tuan Phan <ittphanthetuan@gmail.com>
2024-10-12 21:06:59 +0700

    update

--- docs/New folder/next.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  basePath: "/docs",
-  output: "export",  // <=== enables static exports
-  reactStrictMode: true,
-};
-
-export default nextConfig;
